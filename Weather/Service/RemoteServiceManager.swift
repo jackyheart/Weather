b00f4c5e85adbd96461290a4ffcc862c5e7fcf95
Jacky Tjoa <jacky.tjoa@gmail.com>
2024-09-26 23:15:55 +0800

    move url construction to URLBuilder

--- Weather/Service/RemoteServiceManager.swift
+++ Weather/Service/RemoteServiceManager.swift
@@ -17,18 +17,7 @@ class RemoteServiceManager: RemoteServiceManagerDelegate {
     func fetchCityList(searchString: String, 
                        success: @escaping (SearchResponse?) -> Void,
                        failure: @escaping (Error?) -> Void) {
-        let urlString = buildURL(api: .search, query: searchString)
+        let urlString = URLBuilder.buildURL(api: .search, query: searchString)
         service?.fetchData(urlString: urlString, success: success, failure: failure)
-    }
-    
-    private func buildURL(api: WeatherAPI, query: String) -> String {
-        var baseURL = WeatherAPIConstant.baseURL
-        switch api {
-        case .search:
-            baseURL.append(WeatherAPIEndpoint.search)
-        case .weather:
-            baseURL.append(WeatherAPIEndpoint.weather)
-        }
-        return baseURL + "?key=" + WeatherAPIConstant.key + "&q=" + query + "&format=json"
     }
 }
