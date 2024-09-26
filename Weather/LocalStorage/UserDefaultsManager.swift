0966fbd237b0a3043951f6fa3284904e2bb03605
Jacky Tjoa <jacky.tjoa@gmail.com>
2024-09-26 10:25:40 +0800

    use struct to model 'last viewed' data

--- Weather/LocalStorage/UserDefaultsManager.swift
+++ Weather/LocalStorage/UserDefaultsManager.swift
@@ -8,23 +8,46 @@
 import Foundation
 
 protocol LocalStorageProtocol {
-    func retriveVisitedCities(limit: Int) -> [[String: Any]]?
+    func retriveVisitedCities(limit: Int) -> [LastViewed]
     func persistVisitedCity(value: String)
+}
+
+struct LastViewed: Codable {
+    let displayText: String
+    let viewedDate: Date
 }
 
 class UserDefaultsManager: LocalStorageProtocol {
     let userDefaults = UserDefaults.standard
     let key = "visitedCities"
     
-    func retriveVisitedCities(limit: Int) -> [[String: Any]]? {
-        let storedData = userDefaults.array(forKey: key) as? [[String: Any]]
-        return storedData
+    func retriveVisitedCities(limit: Int) -> [LastViewed] {
+        guard var storedData = userDefaults.object(forKey: key) as? Data else {
+            return []
+        }
+        do {
+            let storedLastVisited = try JSONDecoder().decode([LastViewed].self, from: storedData)
+            //TODO: sort and limit
+            return storedLastVisited
+        } catch {
+            //error handling
+        }
+        return []
     }
     
     func persistVisitedCity(value: String) {
-        let currentDate = Date()
-        var storedData = userDefaults.array(forKey: key) as? [[String: Any]]
-        storedData?.append(["city": value, "date": currentDate])
-        userDefaults.set(storedData, forKey: key)
+        guard var storedData = userDefaults.object(forKey: key) as? Data else {
+            return
+        }
+
+        do {
+            let storedLastVisited = try JSONDecoder().decode([LastViewed].self, from: storedData)
+            let lastViewed = LastViewed(displayText: value, viewedDate: Date())
+            let encodedData = try JSONEncoder().encode(lastViewed)
+            storedData.append(encodedData)
+            userDefaults.set(storedData, forKey: key)
+        } catch {
+            //error handling
+        }
     }
 }
