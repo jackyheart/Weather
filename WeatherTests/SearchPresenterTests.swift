ca388f097e428b5f755ef8eaa161892f6ae59d08
Jacky Tjoa <jacky.tjoa@gmail.com>
2024-09-27 18:44:40 +0800

    add SearchPresenterTests

---
+++ WeatherTests/SearchPresenterTests.swift
@@ -0,0 +1,56 @@
+//
+//  SearchPresenterTests.swift
+//  WeatherTests
+//
+//  Created by Jacky Tjoa on 27/9/24.
+//
+
+import XCTest
+@testable import Weather
+
+final class SearchPresenterTests: XCTestCase {
+    var sut: SearchPresenter!
+    var viewSpy: SearchViewSpy!
+    
+    override func setUp() {
+        viewSpy = SearchViewSpy()
+        sut = SearchPresenter()
+        sut.view = viewSpy
+    }
+    
+    override func tearDown() {
+        viewSpy = nil
+        sut = nil
+    }
+    
+    func testPresentLastViewedCities() {
+        sut.presentLastViewedCities(results: [])
+        //TODO: to update
+        XCTFail()
+    }
+    
+    func testPresentCityList() {
+        sut.presentCityList(results: [])
+        //TODO: to update
+        XCTFail()
+    }
+    
+    func testPresentError() {
+        sut.presentError(error: APIError.dataError)
+        //TODO: to update
+        XCTFail()
+    }
+}
+
+final class SearchViewSpy: SearchViewDelegate {
+    var resultList: [SearchCellModel] = []
+    var errorResult: Error?
+    
+    func displayResultList(_ results: [SearchCellModel]) {
+        resultList = results
+    }
+    
+    func displayErrorAlert(error: Error?) {
+        errorResult = error
+    }
+}
