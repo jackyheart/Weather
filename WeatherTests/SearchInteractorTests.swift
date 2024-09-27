81571c92181fa7c33ee2756f23b2c3137c1f270c
Jacky Tjoa <jacky.tjoa@gmail.com>
2024-09-27 18:53:14 +0800

    add SearchInteractorTests

---
+++ WeatherTests/SearchInteractorTests.swift
@@ -0,0 +1,78 @@
+//
+//  SearchInteractorTests.swift
+//  WeatherTests
+//
+//  Created by Jacky Tjoa on 27/9/24.
+//
+
+import XCTest
+@testable import Weather
+
+final class SearchInteractorTests: XCTestCase {
+    var sut: SearchInteractor!
+    var presenterSpy: SearchPresenterSpy!
+    
+    override func setUp() {
+        presenterSpy = SearchPresenterSpy()
+        sut = SearchInteractor()
+        sut.presenter = presenterSpy
+    }
+    
+    override func tearDown() {
+        presenterSpy = nil
+        sut = nil
+    }
+    
+    func testOnViewLoaded() {
+        //TODO: to update
+        XCTFail()
+    }
+    
+    func testOnSearchEntered() {
+        //TODO: to update
+        XCTFail()
+    }
+    
+    func testDidSelectItem() {
+        //TODO: to update
+        XCTFail()
+    }
+}
+
+final class SearchPresenterSpy: SearchPresenterDelegate {
+    var lastViewedResults: [LastViewedCity] = []
+    var searchResults: [ResultItem] = []
+    var searchError: Error?
+    
+    func presentLastViewedCities(results: [LastViewedCity]) {
+        lastViewedResults = results
+    }
+    
+    func presentCityList(results: [ResultItem]) {
+        searchResults = results
+    }
+    
+    func presentError(error: Error?) {
+        searchError = error
+    }
+}
+
+final class MockRepository: WeatherRepositoryDelegate {
+    var dataStore: [SearchCellModel] = []
+    
+    func storeViewedCity(data: SearchCellModel) {
+    }
+    
+    func retrieveViewedCities(limit: Int) -> [LastViewedCity] {
+        return []
+    }
+    
+    func fetchCityList(searchString: String, 
+                       success: @escaping (SearchResponse?) -> Void,
+                       failure: @escaping ((any Error)?) -> Void) {
+        
+    }
+    
+    func fetchWeather(city: String) {
+    }
+}
