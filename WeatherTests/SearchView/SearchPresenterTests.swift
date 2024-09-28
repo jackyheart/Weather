e92aa2a4f84c871207117058f0264dfd10207ecf
Jacky Tjoa <jacky.tjoa@gmail.com>
2024-09-28 23:32:20 +0800

    update SearchPresenterTests

--- WeatherTests/SearchView/SearchPresenterTests.swift
+++ WeatherTests/SearchView/SearchPresenterTests.swift
@@ -32,25 +32,13 @@ final class SearchPresenterTests: XCTestCase {
     }
     
     func testPresentLastViewedCities() {
-        let inputDates = [
+        let viewDates = [
             "26 Sep 2024 09:48:13 AM",
             "27 Sep 2024 09:47:36 PM",
             "28 Sep 2024 09:31:24 PM"]
         
-        let expectedCityOrder = [
-            "London, United Kingdom",
-            "London, Canada",
-            "Londonderry, United States of America"
-        ]
-        
-        let expectedDateOrder = [
-            "Last viewed: 26 Sep 2024 09:48:13 AM",
-            "Last viewed: 27 Sep 2024 09:47:36 PM",
-            "Last viewed: 28 Sep 2024 09:31:24 PM"
-        ]
-        
         let viewedItems = resultItems.enumerated().map { (index, element) in
-            let dateString = inputDates[index]
+            let dateString = viewDates[index]
             let date = DateUtil.shared.dateFromString(string: dateString)!
             let dataKey = DataModelConverter.constructDataKey(data: element)
             return DataModelConverter.convertDataModelToStorageModel(key: dataKey,
@@ -60,11 +48,23 @@ final class SearchPresenterTests: XCTestCase {
         
         sut.presentLastViewedCities(results: viewedItems)
         
-        let actualCityOrder = viewSpy.cellModels.map { $0.displayText }
-        let actualDateOrder = viewSpy.cellModels.map { $0.noteText }
+        let actualCityResults = viewSpy.cellModels.map { $0.displayText }
+        let actualDateResults = viewSpy.cellModels.map { $0.noteText }
         
-        XCTAssertEqual(actualCityOrder, expectedCityOrder)
-        XCTAssertEqual(actualDateOrder, expectedDateOrder)
+        let expectedCityResults = [
+            "London, United Kingdom",
+            "London, Canada",
+            "Londonderry, United States of America"
+        ]
+        
+        let expectedDateResults = [
+            "Last viewed: 26 Sep 2024 09:48:13 AM",
+            "Last viewed: 27 Sep 2024 09:47:36 PM",
+            "Last viewed: 28 Sep 2024 09:31:24 PM"
+        ]
+        
+        XCTAssertEqual(actualCityResults, expectedCityResults)
+        XCTAssertEqual(actualDateResults, expectedDateResults)
     }
     
     func testPresentSearchedCityList() {
