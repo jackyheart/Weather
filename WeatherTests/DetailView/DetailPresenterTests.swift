27bd87b1284de6d8fdd39ed5d5c816cfea945922
Jacky Tjoa <jacky.tjoa@gmail.com>
2024-09-28 22:03:35 +0800

    add testPresentWeatherResult() on DetailPresenterTests

--- WeatherTests/DetailView/DetailPresenterTests.swift
+++ WeatherTests/DetailView/DetailPresenterTests.swift
@@ -23,6 +23,20 @@ final class DetailPresenterTests: XCTestCase {
         sut = nil
     }
     
+    func testPresentWeatherResult() {
+        let searchResponse: SearchResponse = MockDataManager.fetchMockResponse(fileName: "search")
+        let weatherResponse: WeatherResponse = MockDataManager.fetchMockResponse(fileName: "weather")
+        sut.presentWeatherResult(onDataItem: searchResponse.searchApi.result.first,
+                                 result: weatherResponse.data.currentCondition.first)
+        
+        let resultViewModel = viewSpy.viewModel
+        XCTAssertEqual(resultViewModel?.city, "London")
+        XCTAssertEqual(resultViewModel?.country, "United Kingdom")
+        XCTAssertEqual(resultViewModel?.temperatureCelcius, "15")
+        XCTAssertFalse(resultViewModel?.imageURLString?.isEmpty == true)
+        XCTAssertEqual(resultViewModel?.weatherDescription, "Partly cloudy")
+    }
+    
     func testPresentError() {
         sut.presentError(error: APIError.dataError)
         XCTAssertNotNil(viewSpy.errorResult)
