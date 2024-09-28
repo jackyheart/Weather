b716970cd6420eabcc7302060205f1c7ec4875e4
Jacky Tjoa <jacky.tjoa@gmail.com>
2024-09-28 11:08:15 +0800

    ignore search if text is empty

--- Weather/SearchView/SearchViewController.swift
+++ Weather/SearchView/SearchViewController.swift
@@ -102,7 +102,7 @@ extension SearchViewController: UISearchBarDelegate {
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        guard let searchText = searchBar.text else {
+        guard let searchText = searchBar.text, !searchText.isEmpty else {
             return
         }
         interactor?.onSearchEntered(searchString: searchText)
